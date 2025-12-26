7bc2f20cd21bc3b5de0e821e7c1401a3a49d0204
harusame_sunshower <137337689+harusame-sunshower@users.noreply.github.com>
2025-12-26 17:18:39 +0900

    Update CUBLAS_WORKSPACE_CONFIG for determinism

--- fix_seed/fix_seed.py
+++ fix_seed/fix_seed.py
@@ -1,10 +1,10 @@
 import os
+os.environ.setdefault("CUBLAS_WORKSPACE_CONFIG", ":4096:8")
 import random
 import np
 import torch
 
 def set_deterministic(seed: int = 0, verbose: bool = True):
-    os.environ.setdefault("CUBLAS_WORKSPACE_CONFIG", ":16:8")
     random.seed(seed)
     np.random.manual_seed(seed)
     torch.cuda.manual_seed(seed)
